8c179f8e2411bd23f034f4fa656fa27edfbf9615
Adeola <boluoguna@gmail.com>
2022-09-08 19:10:37 +0100

    made changes to the css and implemented the search country feature

--- src/components/filter.js
+++ src/components/filter.js
@@ -1,21 +1,26 @@
 import React from "react";
- 
 
-const Filter =()=>{
-    return(
-        <form className="form" id="form">
-            <input type="text" name="search" placeholder="Search Country"/>
-            <div>
-                <select name="select" className="select">
-                    <option value="Filter by Region">Filter by Region</option>
-                    <option value="Africa">Africa</option>
-                    <option value="Americas">Americas</option>
-                    <option value="Asia">Asia</option>
-                    <option value="Europe">Europe</option>
-                    <option value="Oceania">Oceania</option>
-                </select>
-            </div>
-        </form>
-    )
-}
+const Filter = ({ searchCountries, searchCountry }) => {
+  return (
+    <form className="form" id="form">
+      <input
+        type="text"
+        name="search"
+        placeholder="Search Country"
+        value={searchCountry}
+        onChange={(e)=>searchCountries(e.target.value)}
+      />
+      <div>
+        <select name="select" className="select">
+          <option value="Filter by Region">Filter by Region</option>
+          <option value="Africa">Africa</option>
+          <option value="Americas">Americas</option>
+          <option value="Asia">Asia</option>
+          <option value="Europe">Europe</option>
+          <option value="Oceania">Oceania</option>
+        </select>
+      </div>
+    </form>
+  );
+};
 export default Filter;
--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -2,19 +2,22 @@ import { useState, useEffect,useContext } from "react";
 import { Link } from "react-router-dom";
 import { FaGlobe } from "react-icons/fa";
 import { ThemeContext } from "../components/ThemeProvider";
+import Filter from "../components/filter";
 
 import axios from "axios";
 
 const Homepage = () => {
   const {darkMode} = useContext(ThemeContext)
-  const [Country, setCountry] = useState([]);
+  const [Countries, setCountries] = useState([]);
   const [isLoading, setLoading] = useState(true);
+  const[filterCountry, setFilterCountry] = useState([]);
+  const[searchCountry, setSearchCountry]= useState("")
 
   const getCountries = () => {
     axios
       .get(`https://restcountries.com/v3.1/all`)
       .then((response) => {
-        setCountry(response.data);
+        setCountries(response.data);
         console.log(response.data);
         setLoading(false);
       })
@@ -26,6 +29,18 @@ const Homepage = () => {
     getCountries();
   }, []);
 
+const searchCountries = (searchValue)=>{
+  setSearchCountry(searchValue);
+  if(searchCountry){
+    const searchedCountry = Countries.filter((country)=>(
+      country.name.common.toLowerCase().includes(searchValue.toLowerCase())
+    ))
+    setFilterCountry(searchedCountry)
+  }
+  else{
+    setFilterCountry(Countries)
+  }
+}
   return (
     <>
       {isLoading ? (
@@ -33,36 +48,73 @@ const Homepage = () => {
           <FaGlobe />
         </div>
       ) : (
+        <section className={darkMode?`homepage countries-dark`:`homepage countries-light`}>
+        <Filter searchCountries={searchCountries} searchCountry={searchCountry}/>
+        {searchCountry.length>0?(
+                <section className="countries-homepage">
+           
+                {filterCountry.map((country) => {
+                  return (
+                    <Link
+                   
+                      to={`${country.cca3.toLowerCase()}`}
+                      key={country.name.common}
+                    >
+                      <div className={darkMode?`countries countries-card-dark`:`countries countries-card-light`}>
+                        <img src={country.flags.png} alt={country.name.common} />
+                        <div className="countries-text">
+                          <h2>{country.name.common}</h2>
+                          <ul>
+                            <li>
+                              <span>Population:</span> {country.population.toLocaleString()}
+                            </li>
+                            <li>
+                              <span>Region:</span> {country.region}
+                            </li>
+                            <li>
+                              <span>Capital:</span> {country.capital}
+                            </li>
+                          </ul>
+                        </div>
+                      </div>
+                    </Link>
+                  );
+                })}
+              </section>
+        ):(
+          <section className="countries-homepage">
+           
+                {Countries.map((country) => {
+                  return (
+                    <Link
+                   
+                      to={`${country.cca3.toLowerCase()}`}
+                      key={country.name.common}
+                    >
+                      <div className={darkMode?`countries countries-card-dark`:`countries countries-card-light`}>
+                        <img src={country.flags.png} alt={country.name.common} />
+                        <div className="countries-text">
+                          <h2>{country.name.common}</h2>
+                          <ul>
+                            <li>
+                              <span>Population:</span> {country.population.toLocaleString()}
+                            </li>
+                            <li>
+                              <span>Region:</span> {country.region}
+                            </li>
+                            <li>
+                              <span>Capital:</span> {country.capital}
+                            </li>
+                          </ul>
+                        </div>
+                      </div>
+                    </Link>
+                  );
+                })}
+              </section>
+        )}
        
-        
-          <section className={darkMode?`countries-homepage countries-dark`:`countries-homepage countries-light`}>
-            {Country.map((country) => {
-              return (
-                <Link
-               
-                  to={`${country.cca3.toLowerCase()}`}
-                  key={country.name.common}
-                >
-                  <div className={darkMode?`countries countries-card-dark`:`countries countries-card-light`}>
-                    <img src={country.flags.png} alt={country.name.common} />
-                    <div className="countries-text">
-                      <h2>{country.name.common}</h2>
-                      <ul>
-                        <li>
-                          <span>Population:</span> {country.population.toLocaleString()}
-                        </li>
-                        <li>
-                          <span>Region:</span> {country.region}
-                        </li>
-                        <li>
-                          <span>Capital:</span> {country.capital}
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
-                </Link>
-              );
-            })}
+    
           </section>
     
       )}
